80fbe4dd82d6eed8eeccf9fcd14f9e467c3be973
rakeshnatra <31076788+rakeshnatra@users.noreply.github.com>
2026-07-26 19:38:05 +1000

    modified urls

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3001";
+  const port = process.env.PORT ?? 3001;
   
     app.enableCors({
     origin: 'http://localhost:3000',
